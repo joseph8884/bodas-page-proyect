fa9ed2286bbea43802342b4d7d601fcad24cea07
joseph8884 <josegzm08@gmail.com>
2024-04-10 19:01:19 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+
+function App() {
+  return (
+    <body>
+      <nav>Barra de buscar: va a ser de JOSE</nav>
+      <header>
+        <h1>Carrusel : Jose</h1>
+      </header>
+      <section className="conoce_mas_frontPage1">
+        <p>Tu vestido de novia una experiencia unica.  ==== Abril</p>
+      </section>
+      <section className="conoce_mas_frontPage2">
+        <p>Encuentra la pieza perfecta que elevara tu look  ==== Abril</p>
+      </section>
+      <section className="conoce_mas_frontPage3">
+        <p>Encuentra la pieza perfecta que elevara tu look  ==== Abril</p>
+      </section>
+      <section className="Galeria_fotos_frontPage">
+        <p>Gaeleria de fotos: Mateo</p>
+      </section>
+      <section className="formulario_contacto">
+        <p>Contactanos : Mateo</p>
+      </section>
+      <footer>
+        <p>Foooter: Parte de santafe</p>
+      </footer>
+    </body>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './style/main.css';
+import './style/nav.css';
+import './style/frontPage/Carrusel.css'
+import './style/frontPage/conoce_mas.css'
+import './style/frontPage/galeria.css'
+import './style/frontPage/form_contactanos.css'
+import './style/footer.css'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
