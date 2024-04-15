043e42adc0f4386b342682e01b1445482d15b49a
Juan Abril <juanabca@unisabana.edu.co>
2024-04-15 11:40:28 -0500

    section conoce mas

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
+import img3 from './img/img3.png';
 
 function App() {
   return (
     <body>
       <nav>Barra de buscar: va a ser de JOSE</nav>
       <header>
-        <h1>Carrusel : Jose</h1>
+        <p>Carrusel : Jose</p>
       </header>
-      <section className="conoce_mas_frontPage1">
-        <p>Tu vestido de novia una experiencia unica.  ==== Abril</p>
-      </section>
-      <section className="conoce_mas_frontPage2">
-        <p>Encuentra la pieza perfecta que elevara tu look  ==== Abril</p>
-      </section>
-      <section className="conoce_mas_frontPage3">
-        <p>Encuentra la pieza perfecta que elevara tu look  ==== Abril</p>
-      </section>
+
+      <div className="grid-layout1">
+          <section className="conoce_mas_frontPage1">
+              <h2>TU VESTIDO ES LA EXPRESIÓN DE LA IMAGEN QUE QUIERES PROYECTAR!</h2>
+              <h1>TU VESTIDO DE</h1>
+              <h1>NOVIA UNA</h1>
+              <h1>EXPERIENCIA ÚNICA</h1>
+              <br/>
+              <p>El vestido habla de ti, de tu personalidad de tu estilo. Cada detalle, desde la silueta que eligas
+                  hasta los adornos, contribuyen a la narrativa visual de este dia tan especial.</p>
+              <br/>
+              <p>Aqui encontrarás la imagen que quieres recordar y trascenderá en la mente de tus generaciones!</p>
+              <br/>
+              <button>CONOCE<br/>MÁS</button>
+          </section>
+          <section className="imagen1" >
+          <img className="img1" src={img1}/>
+          </section>
+      </div>
+
+      <div className="grid-layout2">
+          <section className="imagen2">
+              <img className="img2" src={img2}/>
+          </section>
+          <section className="conoce_mas_frontPage2">
+              <h2>ACCESORIOS</h2>
+              <h1>¡ENUCENTRA LA PIEZA PERFECTA QUE ELEVARÁ TU LOOK!</h1>
+              <br/>
+              <p>Los accesorios expresan tu creatividad y gusto personal; agregan estilo, personalidad y originalidad a tus outfits.</p>
+              <br/>
+              <button>CONOCE<br/>MÁS</button>
+          </section>
+      </div>
+
+      <div className="grid-layout3">
+        <section className="conoce_mas_frontPage3">
+            <h2>DESCUBRE COMO DESTACAR EN TU PROXIMA FIESTA</h2>
+            <h1>¡BRILLA CON EL VESTIDO PERFECTO!</h1>
+            <br/>
+            <p>Descubre una amplia seleccion de vestidos elegantes y sofisticados que te ayudarán a destacar y brillar en tu proxima fiesta.</p>
+            <br/>
+            <p>Encuentra el vestido perfecto para cada ocasion y haz una entrada inolvidable</p>
+            <br/>
+            <button>CONOCE<br/>MÁS</button>
+        </section>
+          <section className="imagen3">
+              <img className="img3" src={img3}/>
+          </section>
+      </div>
+
       <section className="Galeria_fotos_frontPage">
         <p>Gaeleria de fotos: Mateo</p>
       </section>
